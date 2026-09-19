7453a13ecdaa17731be677adf1a2cbce4ce91aef
Nusrat Jahan Mim <njmim01627@gmail.com>
2026-09-19 13:34:04 +0600

    Add variable declarations and print statements

---
+++ 04_variables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    float marks;
+    char grade;
+
+    age = 20;
+    marks = 85.5;
+    grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Marks: %.2f\n", marks);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
