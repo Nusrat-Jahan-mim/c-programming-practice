ef29b9f6ac04764ec79de08c5f7f5ffbf01c1977
Nusrat Jahan Mim <njmim01627@gmail.com>
2026-09-19 13:38:30 +0600

    Add format specifiers example in C

---
+++ 08_format_specifiers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int number = 10;
+    float decimal = 10.5;
+    double value = 20.12345;
+    char letter = 'M';
+
+    printf("Integer: %d\n", number);
+    printf("Float: %f\n", decimal);
+    printf("Double: %lf\n", value);
+    printf("Character: %c\n", letter);
+
+    return 0;
+}
