1c52bd7ef5f069fe1db40a58193b512190a2a91a
Nusrat Jahan Mim <njmim01627@gmail.com>
2026-09-19 13:32:06 +0600

    Add comments example in C program

---
+++ 02 — Comments.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    // This is a single-line comment
+
+    /*
+       This is a
+       multi-line comment
+    */
+
+    printf("Comments in C");
+
+    return 0;
+}
